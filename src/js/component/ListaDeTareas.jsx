58479d002bd35b44f6125c2ee3f721e1256b2c3e
ihorSmachylo <igoor.smachyloo@gmail.com>
2023-02-06 15:38:11 +0000

    Todo list ihor

---
+++ src/js/component/ListaDeTareas.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import TareaFormulario from "./TareaFormulario.jsx";
+import Tarea from './Tarea.jsx'
+import '../../styles/ListaDeTareas.css'
+
+function ListaDeTareas() {
+
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = tarea => {
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+  }
+
+    const eliminarTarea = id =>{
+      const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+      setTareas(tareasActualizadas);
+    }
+
+  return (
+    <>
+      <TareaFormulario onSubmit={agregarTarea} />
+      <div className="tareas-lista-contenedor">
+        {
+          tareas.map((tarea) =>
+            <Tarea
+              key={tarea.id}
+              id={tarea.id}
+              texto={tarea.texto}
+              eliminarTarea={eliminarTarea}
+            />
+          )
+        }
+      </div>
+    </>
+  )
+}
+export default ListaDeTareas;
---
+++ src/js/component/Tarea.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../../styles/Tarea.css'
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+function Tarea ({id,texto,completada,completarTarea,eliminarTarea}){
+  return(
+    <div className='tarea-contenedor'>
+      <div className="tarea-texto"
+      onClick={ () => completarTarea(id)}>
+        {texto}
+      </div>
+      <div className="tarea-contendor-iconos"
+      onClick={ () => eliminarTarea(id) }>
+        <AiOutlineCloseCircle className="tarea-icono"/>
+      </div>
+    </div>
+  )
+
+}
+export default Tarea;
